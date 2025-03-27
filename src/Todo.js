d4192b1a9e2cdc6f8051b47571d02b7499239c20
Nandeswar <nandeswarmyneni@gmail.com>
2025-03-27 17:04:06 +0530

    TodowithBackendAPI's

--- src/Todo.js
+++ src/Todo.js
@@ -1,50 +1,66 @@
-import { useState,useEffect } from "react";
+import { useState, } from "react";
 import TodoIteams from "./TodoIteams";
 import "./App.css";
+import axios from "axios";
 
 const Todolist = () => {
-    const [task, setTask] = useState("");
-    const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
 
-     useEffect(() => {
-        const storedTodos = localStorage.getItem("todos");
-        setTodos(storedTodos ? JSON.parse(storedTodos) : []);
-      }, []);
+//   useEffect(() => {
+//     fetchTodos();
+//   }, []);
 
-       
-      useEffect(()=>{
-        localStorage.setItem("todos",JSON.stringify(todos));
-      },[todos]);
+//   const fetchTodos = () => {
+//     axios
+//       .get("http://localhost:8080/ToDoIteams/getAllTodos") 
+//       .then((response) => {
+//         setTodos(response.data);
+//       })
+     
+//   };
 
-    const changeHandler = e => {
-        setTask(e.target.value);
-    };
+  const changeHandler = (e) => {
+    setTask(e.target.value);
+  };
 
-    const submitHandler = e =>{
-        e.preventDefault();
-        const newTodos = [...todos,task];
-        setTodos(newTodos);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (task.trim() === "") return;
+    axios
+      .post("http://localhost:8080/ToDoIteams/saveTodoTask", {
+        todoTask: task,
+      })
+      .then((response) => {
+        setTodos([...todos, response.data]);
         setTask("");
-    };
+      })
+      
+  };
 
-    const deleteHandler = (indexValue) => {
-        const newTodos = todos.filter((_todo, index) => index !== indexValue); 
-        setTodos(newTodos);
-    };
+  const deleteHandler = (id) => {
+    axios
+      .delete(`http://localhost:8080/ToDoIteams/deletTodo/${id}`)
+      .then(() => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    
+      })
+     
+  };
 
 
-    return (
-        
-        <div className="container">
-            <h1>Things To Do Daily</h1>
-            <form onSubmit={submitHandler}>
-                <input type="text" placeholder="Enter Your Things To Do" value={task} onChange={changeHandler} />
-                <button type="submit">ADD</button>
-            </form>
-            <TodoIteams todos={todos} deleteHandler={deleteHandler} />
-        </div>
-        
-    );
+ 
+
+  return (
+    <div className="container">
+      <h1>Things To Do Daily</h1>
+      <form onSubmit={submitHandler}>
+        <input type="text" placeholder="Enter Your Things To Do" value={task} onChange={changeHandler}/>
+        <button type="submit">ADD</button>
+      </form>
+      <TodoIteams todos={todos} deleteHandler={deleteHandler}  />
+    </div>
+  );
 };
 
 export default Todolist;
--- src/TodoIteams.js
+++ src/TodoIteams.js
@@ -1,15 +1,23 @@
+import { Alert } from "bootstrap";
 import React from "react";
 
-const TodoIteams = ({ todos, deleteHandler }) => {
-    return (
-        <div>
-            {todos.map((todo, index) => (
-                <div key={index}>
-                    <h2>{todo} <button onClick={() => deleteHandler(index)}>DELETE</button></h2>
-                </div>
-            ))}
+const TodoIteams = ({ todos, deleteHandler,  }) => {
+
+
+  return (
+    <div>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <h2>
+            {todo.todoTask}
+            <button
+              onClick={() => {
+                deleteHandler(todo.id,) }}>DELETE </button>
+          </h2>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default TodoIteams;
